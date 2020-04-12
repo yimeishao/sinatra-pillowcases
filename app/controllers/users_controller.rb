eaae716c0a850bfa1f62f9d95499e0448a4f8703
yimeishao <maria.yimei.shao@gmail.com>
2020-04-12 00:41:53 -0600

    add user id to session hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,19 @@ end
 
 post '/users' do 
     user = User.create(params)
+    session[:user_id] = user.id
+    #adding user id to the session hash 
+    #this allows us to check which user is logged in
+    #when the user logs out, clear the entire session hash
     redirect to "/profile/#{user.id}"
 end 
 
 get '/profile/:id' do 
+    if User.find_by(id: params[:id])
     @user = User.find_by(id: params[:id])
+    else 
+        redirect to "/"
+    end 
     erb :"users/profile"
 end 
 end 
