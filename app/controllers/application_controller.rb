a0db24ef1703fc30ecf1c9a2454e071f6aaed7f3
yimeishao <maria.yimei.shao@gmail.com>
2020-04-12 00:16:34 -0600

    created signup page and method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions #this is a hash 
+    set :session_secret, "secret"
+    #need to manually include this when using sinatra and shotgun 
+    #otherwise the session would automatically clear with each new request
   end
 
   get "/" do
