9b88c67edd9427dfc6a715509771f879ac5f8cba
yimeishao <maria.yimei.shao@gmail.com>
2020-04-12 00:35:56 -0600

    set up redirect to profile view from signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,13 @@ get '/signup' do
     erb :"users/signup"
 end 
 
+post '/users' do 
+    user = User.create(params)
+    redirect to "/profile/#{user.id}"
+end 
+
+get '/profile/:id' do 
+    @user = User.find_by(id: params[:id])
+    erb :"users/profile"
+end 
 end 
